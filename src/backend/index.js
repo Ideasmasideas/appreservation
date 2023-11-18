dea89209bc052e01f5fa3c6f5734478f5bfcb753
José Manzano Hartl <jose@ideasmasideas.com>
2023-11-18 22:31:09 +0100

    data base rooms

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,33 +1,33 @@
 const express = require('express');
-//const { Pool } = require('@vercel/postgres');
 const { Pool } = require('pg');
 
 const app = express();
 
 // Configura la conexión a PostgreSQL
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  user: 'useraws',
+  host: 'roomaws.cgwc0bqmobtd.eu-west-3.rds.amazonaws.com',
+  database: 'dbreservationrooms',
+  password: 'KaraokeRoom',
+  port: 5432, // Puerto por defecto de PostgreSQL
 });
 
 // Ruta para obtener los slots desde la base de datos
 app.get('/slots', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM rooms'); // Modifica la consulta según tu estructura de base de datos
-        const slots = result.rows;
-        client.release();
-        console.log(res.json(slots));
-    } catch (err) {
-        console.error('Error al obtener slots', err);
-        res.status(500).json({ error: 'Error al obtener slots' });
-    }
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM rooms'); // Modifica la consulta según tu estructura de base de datos
+    const slots = result.rows;
+    client.release();
+    res.json(slots);
+  } catch (err) {
+    console.error('Error al obtener slots', err);
+    res.status(500).json({ error: 'Error al obtener slots' });
+  }
 });
 
 // Puerto en el que el servidor Express escuchará las solicitudes
-const PORT = process.env.PORT || 3009;
+const PORT = 3008;
 app.listen(PORT, () => {
-    console.log(`Servidor Express corriendo en el puerto ${PORT}`);
+  console.log(`Servidor Express corriendo en el puerto ${PORT}`);
 });
